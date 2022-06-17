4a1c33697dc24be1c940faae043eff4eceee1f50
Kaliandra727 <kfreitaspereira1@gmail.com>
2022-06-17 12:36:44 -0400

    Add project files.

---
+++ Atacado.Dal/Estoque/ProdutoDao.cs
@@ -0,0 +1,59 @@
+﻿using Atacado.Dal.Ancestral;
+using Atacado.EF.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atacado.Dal.Estoque
+{
+    public class ProdutoDao : BaseAncestralDao<Produto>
+    {
+       public ProdutoDao() : base()
+       { }
+
+        public override Produto Create(Produto obj)
+        {
+            this.contexto.Produtos.Add(obj);
+            this.contexto.SaveChanges();
+            return obj;
+        }
+
+        public override Produto Read(int id)
+        {
+            Produto obj = this.contexto.Produtos.SingleOrDefault(prod => prod.IdProduto == id);
+            return obj;
+        }
+
+        public override List<Produto> ReadAll()
+        {
+            return this.contexto.Produtos.ToList();
+        }
+
+        public override Produto Update(Produto obj)
+        {
+            Produto alt = this.Read(obj.IdProduto);
+            alt.DescricaoProduto = obj.DescricaoProduto;
+            alt.Situacao = obj.Situacao;
+            alt.IdCategoria = obj.IdCategoria;
+            alt.IdSubcategoria = obj.IdSubcategoria;
+            this.contexto.SaveChanges();
+            return alt;
+        }
+
+        public override Produto Delete(int id)
+        {
+            Produto del = this.Read(id);
+            this.contexto.Produtos.Remove(del);
+            this.contexto.SaveChanges();
+            return del;
+        }
+
+        public override Produto Delete(Produto obj)
+        {
+            return this.Delete(obj.IdProduto);
+        }
+
+    }
+}
---
+++ Atacado.Dal/Estoque/SubcategoriaDao.cs
@@ -0,0 +1,56 @@
+﻿using Atacado.Dal.Ancestral;
+using Atacado.EF.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atacado.Dal.Estoque
+{
+    public class SubcategoriaDao : BaseAncestralDao<Subcategoria>
+    {
+        public SubcategoriaDao() : base()
+        { }
+
+        public override Subcategoria Create(Subcategoria obj)
+        {
+            this.contexto.Subcategorias.Add(obj);
+            this.contexto.SaveChanges();
+            return obj;
+        }
+        public override Subcategoria Read(int id)
+        {
+            Subcategoria obj = this.contexto.Subcategorias.SingleOrDefault(subc => subc.IdSubcategoria == id);
+            return obj;
+        }
+
+        public override List<Subcategoria> ReadAll()
+        {
+            return this.contexto.Subcategorias.ToList();
+        }
+
+        public override Subcategoria Update(Subcategoria obj)
+        {
+            Subcategoria alt = this.Read(obj.IdSubcategoria);
+            alt.DescricaoSubcategoria = obj.DescricaoSubcategoria;
+            alt.Situacao = obj.Situacao;
+            alt.IdCategoria = obj.IdCategoria;
+            this.contexto.SaveChanges();
+            return alt;
+        }
+
+        public override Subcategoria Delete(int id)
+        {
+            Subcategoria del = this.Read(id);
+            this.contexto.Subcategorias.Remove(del);
+            this.contexto.SaveChanges();
+            return del;
+        }
+
+        public override Subcategoria Delete(Subcategoria obj)
+        {
+            return this.Delete(obj.IdSubcategoria);
+        }
+    }
+}
